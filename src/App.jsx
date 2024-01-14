51758fe18f9b1351ce2a5deb4680f0f2a3655398
Tyler Prehl <tyler@prehl.us>
2024-01-13 23:54:04 -0500

    Revise App.jsx to offer specific routes to Spinach or Popeye. ErrorPage is displayed (as expected) if only 'profile' is visited)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import { Link } from "react-router-dom";
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -26,7 +26,10 @@ function App() {
         <nav>
           <ul>
             <li>
-              <Link to="profile">Profile page</Link>
+              <Link to="profile/spinach">Spinach page</Link>
+            </li>
+            <li>
+              <Link to="profile/popeye">Popeye page</Link>
             </li>
           </ul>
         </nav>
