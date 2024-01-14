f02bde48985f060bef29c9db6271c083fb1fc3f6
Tyler Prehl <tyler@prehl.us>
2024-01-14 00:30:05 -0500

    Update README with Fetching Data in React

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Router from './components/Router'
+// import Router from './router-components/Router'
+import Image from './fetch-data-components/Image'
 
 import './index.css'
 
+// to see the page created for the Router lesson, display 'Router'
+// to see the page created for the Data Fetching lesson, display 'Image'
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router />
+    <Image />
   </React.StrictMode>
 );
